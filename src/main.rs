cfc17f8c3334fc794d6d9881ab05d23c9fee187a
Nathan Shelly <nsnshelly@gmail.com>
2020-10-24 02:20:47 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::env;
 use std::fs;
+use std::path::Path;
 
 fn main() {
-  let filename = "./resources/one_song.md";
+  let path = Path::new("./resources/one_song.md");
 
-  println!("In file {}", filename);
-
-  let contents = fs::read_to_string(filename)
-    .expect("Something went wrong reading the file");
+  let contents =
+    fs::read_to_string(path).expect("Something went wrong reading the file");
 
   println!("With text:\n{}", contents);
 }

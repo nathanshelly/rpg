b0dd6f3ea9296ff4e24ec111a0191eb3e0271096
Nathan Shelly <nsnshelly@gmail.com>
2020-10-24 02:15:21 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
+
 fn main() {
-  println!("Hello, world!");
+  let filename = "./resources/one_song.md";
+
+  println!("In file {}", filename);
+
+  let contents = fs::read_to_string(filename)
+    .expect("Something went wrong reading the file");
+
+  println!("With text:\n{}", contents);
 }

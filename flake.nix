0420fa48290351972fcfbb6c3574b45355947882
Nathan Shelly <nsnshelly@gmail.com>
2020-10-31 22:37:04 -0700

    wip

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+    defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.hello;
+
+  };
+}

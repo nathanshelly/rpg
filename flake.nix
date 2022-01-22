cd38adfd782edabffb4d2c97b9478e31441d5636
Nathan Shelly <nsnshelly@gmail.com>
2022-01-21 21:01:41 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   # Provides abstraction to boiler-code when specifying multi-platform outputs.
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.mozilla.url = "github:mozilla/nixpkgs-mozilla";
-  inputs.mozilla.flake = false;
+  # inputs.mozilla.url = "github:mozilla/nixpkgs-mozilla";
+  # inputs.mozilla.flake = false;
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
@@ -27,7 +27,7 @@
               buildInputs = [
                 pkgs.rust-analyzer
                 pkgs.cargo-watch
-                rust
+                # rust
               ];
             };
           }
